68fa60b9abaac1ac0e4d8e68a78b194711e7472f
Patrick McNeal <patrick.mcneal@gmail.com>
2012-05-21 16:40:14 -0400

    Validate user fields.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120518173906) do
+ActiveRecord::Schema.define(:version => 20120521203419) do
 
   create_table "trials", :force => true do |t|
     t.integer  "user_id"
@@ -29,5 +29,7 @@ ActiveRecord::Schema.define(:version => 20120518173906) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
