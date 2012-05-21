895e11117ad2178695e558cca6efdf367bb953e4
Patrick McNeal <patrick.mcneal@gmail.com>
2012-05-21 16:01:26 -0400

    Annotate models.

--- app/models/trial.rb
+++ app/models/trial.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: trials
+#
+#  id                 :integer         not null, primary key
+#  user_id            :integer
+#  title              :string(255)
+#  published          :boolean
+#  published_datetime :datetime
+#  created_at         :datetime        not null
+#  updated_at         :datetime        not null
+#
+
 class Trial < ActiveRecord::Base
   attr_accessible :published, :published_datetime, :title, :user_id
   
