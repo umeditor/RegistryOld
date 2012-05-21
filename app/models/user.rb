52ad2888d7c06013a38e3466c5b70dcfc456059e
Patrick McNeal <patrick.mcneal@gmail.com>
2012-05-21 16:53:49 -0400

    Add secure passwords.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,9 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :affiliation, :email, :name
-  
+  attr_accessible :affiliation, :email, :name, :password, :password_confirmation
+  has_secure_password
+
   before_save { |user| user.email = email.downcase }
 
   has_many :trials
@@ -20,5 +21,8 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+    uniqueness: { case_sensitive: false }
+  validates :affiliation, length: { maximum: 50 }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,20 @@ require 'spec_helper'
 
 describe User do
   before do
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com", affiliation: "Example University",
+    password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:affiliation) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
+
 
   it { should be_valid }
 
@@ -20,6 +27,11 @@ describe User do
   describe "when email is not present" do
     before { @user.email = " " }
     it { should_not be_valid }
+  end
+
+  describe "when affiliation is not present" do
+    before { @user.affiliation = " " }
+    it { should be_valid }
   end
 
   describe "when name is too long" do
@@ -64,5 +76,46 @@ describe User do
     end
 
     it { should_not be_valid }
+  end
+
+  describe "when affiliation is too long" do
+    before { @user.affiliation = "a" * 51 }
+    it { should_not be_valid }
+  end
+
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
+    it { should_not be_valid }
+  end
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by_email(@user.email) }
+
+    describe "with valid password" do
+      it { should == found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not == user_for_invalid_password }
+      specify { user_for_invalid_password.should be_false }
+    end
   end
 end
